e1e8d0b3f3a3a8c735f2e75b27a6734868c36af6
whynot <aminfury@mail.ru>
2024-05-15 17:36:38 +0300

    dockerfile & axios url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
-const BASE_URL = 'http://localhost:8080/api/'
-// const BASE_URL = 'http://192.168.0.134:8080/api/'
+// const BASE_URL = 'http://localhost:8080/api/'
+const BASE_URL = 'https://e-commerce-spring.onrender.com/api'
 
 
 export default axios.create({
