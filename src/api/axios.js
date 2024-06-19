11b629137d4c988c260d1bec4374142e39830770
whynot <aminfury@mail.ru>
2024-06-20 01:19:51 +0300

    api url back to remote server

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
-const BASE_URL = 'http://localhost:8080/api'
-// const BASE_URL = 'https://e-commerce-spring.onrender.com/api'
+// const BASE_URL = 'http://localhost:8080/api'
+const BASE_URL = 'https://e-commerce-spring.onrender.com/api'
 
 
 export default axios.create({
